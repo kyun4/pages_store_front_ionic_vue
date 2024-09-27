8deab3aaf955130ae76931a8c79fb56e12fc93f4
kyun04 <efrencamposagrado0622@gmail.com>
2024-09-27 12:59:17 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pages Store Front',
+  webDir: 'dist'
+};
+
+export default config;
